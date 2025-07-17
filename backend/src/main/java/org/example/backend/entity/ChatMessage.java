0fddedee09caf5d32ce29ea3e08dd4531e5d4456
Kimkihyun/김기현 <FIFLove1126@gmail.com>
2025-07-17 09:32:56 +0900

    [MBA-29] fix : ChatMessage
    Builder 패턴을 추가하였습니다.

--- backend/src/main/java/org/example/backend/entity/ChatMessage.java
+++ backend/src/main/java/org/example/backend/entity/ChatMessage.java
@@ -1,6 +1,9 @@
 package org.example.backend.entity;
 
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
@@ -8,6 +11,7 @@ import java.time.LocalDateTime;
 @Entity
 @Getter
 @Table(name = "chat_message")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class ChatMessage extends BaseTimeEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,4 +26,12 @@ public class ChatMessage extends BaseTimeEntity {
 
     private Boolean isRead;
     private LocalDateTime sendAt;
+
+    @Builder
+    public ChatMessage(ChatRoom chatRoom, String message) {
+        this.chatRoom = chatRoom;
+        this.message = message;
+        this.isRead = false;
+        this.sendAt = LocalDateTime.now();
+    }
 }
