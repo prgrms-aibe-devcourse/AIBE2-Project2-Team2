e19c9363f51faa0ab826acc9e4df43d1cc0c6e1f
Kimkihyun/김기현 <FIFLove1126@gmail.com>
2025-07-17 10:11:56 +0900

    [MBA-59] fix : ChatMessageRepository
    interface 메서드 추가

--- backend/src/main/java/org/example/backend/repository/ChatMessageRepository.java
+++ backend/src/main/java/org/example/backend/repository/ChatMessageRepository.java
@@ -1,7 +1,12 @@
 package org.example.backend.repository;
 
 import org.example.backend.entity.ChatMessage;
+import org.example.backend.entity.ChatRoom;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ChatMessageRepository extends JpaRepository<ChatMessage, Long> {
+    // 특정 채팅방 메시지 전부 가져오기
+    List<ChatMessage> findByChatRoomOrderBySendAtAsc(ChatRoom chatRoom);
 }
