597e5963955847466d5d26dc0db0b4f23c488579
Kimkihyun/김기현 <FIFLove1126@gmail.com>
2025-07-17 11:14:05 +0900

    [MBA-58] fix : ChatRoomRepository

--- backend/src/main/java/org/example/backend/repository/ChatRoomRepository.java
+++ backend/src/main/java/org/example/backend/repository/ChatRoomRepository.java
@@ -1,7 +1,16 @@
 package org.example.backend.repository;
 
 import org.example.backend.entity.ChatRoom;
+import org.example.backend.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+    // 특정 두 멤버의 방이 있는지 확인
+    Optional<ChatRoom> findByMember1AndMember2(Member member1, Member member2);
+
+    // 특정 멤버가 참여한 방 전부 조회
+    List<ChatRoom> findByMember1OrMember2(Member member1, Member member2);
 }
