ba232a97cc565885e725dec776c99b128eadfe67
springbbot-developer <swmoon7@naver.com>
2025-07-18 17:54:00 +0900

    [MBA-43] fix : 포트폴리오 엔티티수정

--- backend/src/main/java/org/example/backend/entity/Portfolio.java
+++ backend/src/main/java/org/example/backend/entity/Portfolio.java
@@ -27,13 +27,25 @@ public class Portfolio {
     @Column(columnDefinition = "TEXT")
     private String content;
 
+    @Column(name = "view_count", nullable = false)
+    private Long viewCount;
+
+    @Column(name ="working_year", nullable = false)
+    private Integer workingYear;
+
+    @Column(name = "category", nullable = false)
+    private String category;        //DetailField에 속한 내용으로 이름만 가져온다.
+
     @OneToMany(mappedBy = "portfolio", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<PortfolioImage> images = new ArrayList<>();
 
-    public Portfolio(ExpertProfile expertProfile, String title, String content) {
+    public Portfolio(ExpertProfile expertProfile, String title, String content, Integer workingYear, String category) {
         this.expertProfile = expertProfile;
         this.title = title;
         this.content = content;
+        this.viewCount = 0L; // 초기 조회수는 0
+        this.workingYear = workingYear;
+        this.category = category;
     }
 
 }
