ccd54a495d9b0a0cd3dae81600e82e072f4986d6
gyuuu9933 <gyuuu9933@gmail.com>
2025-07-18 11:23:33 +0900

    [MBA-74] feat : content image 엔티티

---
+++ backend/src/main/java/org/example/backend/entity/ContentImage.java
@@ -0,0 +1,23 @@
+package org.example.backend.entity;
+
+import lombok.Getter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Table(name = "content_img")
+public class ContentImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long contentImageid;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "content_id", nullable = false)
+    private Content content;
+
+    private String imageUrl;
+
+    @Column(name = "order_index", nullable = false)
+    private byte orderIndex;
+}
