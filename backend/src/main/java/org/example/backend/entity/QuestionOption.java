e76e0623aad175b4b17b6bc7ca7a09d36ce9822f
gyuuu9933 <gyuuu9933@gmail.com>
2025-07-24 18:04:01 +0900

    [MBA-76] feat : 컨텐츠 옵션 선택 구현 엔티티

--- backend/src/main/java/org/example/backend/entity/QuestionOption.java
+++ backend/src/main/java/org/example/backend/entity/QuestionOption.java
@@ -24,11 +24,19 @@ public class QuestionOption {
     private String optionText;
 
     @Column(name = "additional_price", nullable = false)
-    private String additionalPrice;
+    private Long additionalPrice = 0L; // 추가금 기본 설정 0원으로 -> 옵션별 0원인 경우에도 사용할 수 있도록
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "question_id", nullable = false)
     @JsonBackReference
     private Question question;
 
+    public QuestionOption(String optionText, Long additionalPrice) {
+        this.optionText = optionText;
+        this.additionalPrice = additionalPrice;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
 }
