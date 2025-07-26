ec7a102a2a32b71b15a611b3ad790c19df13fd89
rmsmf0 <138198521+rmsmf0@users.noreply.github.com>
2025-07-27 02:38:42 +0900

    feat: findByReview 추가

--- backend/src/main/java/org/example/backend/repository/ReviewImageRepository.java
+++ backend/src/main/java/org/example/backend/repository/ReviewImageRepository.java
@@ -1,7 +1,11 @@
 package org.example.backend.repository;
 
+import org.example.backend.entity.Review;
 import org.example.backend.entity.ReviewImage;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewImageRepository extends JpaRepository<ReviewImage, Long> {
+    List<ReviewImage> findByReview(Review review);
 }
